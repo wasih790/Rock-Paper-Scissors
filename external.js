c0f01d980b10d3c874a052414ca3c1843cf1d467
wasih790 <120667351+wasih790@users.noreply.github.com>
2023-02-15 17:18:21 -0500

    Changed the output to change for each click
    the output of the play round function will change
    automatically when each new click of the button is pressed.

--- external.js
+++ external.js
@@ -13,17 +13,6 @@
         }
     }
 
-    // function getPlayerChoice(){
-    //     let value = prompt("Please Enter a value, Rock, Paper or Scissors. \nThe Prompt will be called 5 times for 5 rounds. \nCheck Console screen after.");
-    //     if (!(value === "rock"|| value === "Rock"|| value === "RocK" || value === "ROCK" || value === "paper" || value === "Paper" || value === "PapeR" || value === "PAPER" || value === "scissors" || value === "Scissor" || value === "Scissors"|| value === "scissor" || value === "ScissorS" || value === "SCISSORS")){
-    //     return getPlayerChoice();
-    //     }
-    //     else{
-    //         return value.charAt(0).toUpperCase() + value.substr(1).toLowerCase();}
-        
-    // }
-
-
     function playRound(playerSelection, computerSelection) {
             if ((playerSelection === "Rock" &&  computerSelection === "Rock") || 
             (playerSelection === "Paper" &&  computerSelection === "Paper") ||
@@ -44,63 +33,14 @@
 
 
 
-    // function game(){
-    //     let compwin = 0;
-    //     let playerwin = 0;
-    //         for (let i = 0; i < 5; i++){
-    //             console.log(`Round ${i+1}`);
-    //             let computerSelection = getComputerChoice(randNum());
-    //             let playerSelection = getPlayerChoice();
-    //             console.log(`Computer Choice: ${computerSelection}`);
-    //             console.log(`Player Choice: ${playerSelection}`);
-    //             let match = playRound(playerSelection, computerSelection);
-    //             if (match === 0){
-    //                 console.log("Tie for the round")
-    //             }
-    //             else if (match === 1){
-    //                 console.log (`Computer Wins the round, You Lose! ${computerSelection} beats ${playerSelection}`);
-    //                 compwin++;
-    //             }
-    //             else if (match === 2){
-    //                 console.log(`You Win the round, Computer Loses! ${playerSelection} beats ${computerSelection}`);
-    //                 playerwin++;
-    //             }
-
-    //         }
-    //         if (compwin > playerwin){
-    //        console.log(`%c Computer wins the game with ${compwin} wins against ${playerwin}`, `font-weight: 900; font-size: 20px;color: red; text-shadow: 3px 3px 0 8px;  text-align: center`)
-    //         }
-    //         else if (playerwin > compwin){
-    //             console.log(`%c Player wins the game with ${playerwin} wins against ${compwin}`, `font-weight: 900; font-size: 20px;color: red; text-shadow: 3px 3px 0 8px; text-align: center`)
-    //         }
-    //         else{
-    //             console.log(`%c It is a Tie with ${playerwin} for the player and ${compwin} for computer`, `font-weight: 900;  font-size: 20px;color: red; text-shadow: 3px 3px 0 8px; text-align: center;`)
-    //         }
-    //     }
-    
-// console.log(game());
-
-
-
-
-// const btn = document.querySelector('#Rock');
-// btn.addEventListener('click', () => {console.log(playRound('Rock',computerSelection));});
-
 
 const buttons = document.querySelectorAll('button');
 
 // we use the .forEach method to iterate through each button
 buttons.forEach((button) => {
   button.addEventListener('click', () => {
-    const output = document.querySelector('#output');
-    const content = document.createElement('div');
-    content.classList.add('content');
-    content.textContent = `${playRound(button.id,getComputerChoice(randNum()))}`;
-    output.appendChild(content);
-
+    playerSelection = button.id;
+    computerSelection = getComputerChoice(randNum())
+    document.getElementById('output').textContent =  playRound(playerSelection,computerSelection);
   })
-
-
-
-
 });
