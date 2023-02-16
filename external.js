48f3818c829e64440bebb6f03844d5a638bd4e8b
wasih790 <120667351+wasih790@users.noreply.github.com>
2023-02-16 16:28:52 -0500

    Added a button to restart the game
    the css was added as well, the page is just reloaded when the button
    is pressed
    and moved some code into a function, so it is easier to see.

--- external.js
+++ external.js
@@ -32,10 +32,6 @@
     }
 
 
-    // return("Tie for the round");
-    // return (`Computer Wins the round, You Lose! ${computerSelection} beats ${playerSelection}`);
-    // return(`You Win the round, Computer Loses! ${playerSelection} beats ${computerSelection}`)
-
 
 function wins(num, computerSelection, playerSelection){
     if(num === 0){
@@ -43,14 +39,14 @@ function wins(num, computerSelection, playerSelection){
        
     }
     else if (num === 1){
-        ++compWin;
-       return( document.getElementById('output').textContent = `Computer Wins the round, You Lose! ${playerSelection} beats ${computerSelection}`);
+        compWin++;
+       return( document.getElementById('output').textContent = `Computer Wins the round, You Lose! ${playerSelection} beats ${computerSelection}.`);
         
       
     }
     else if (num === 2){
-        ++playWin;
-        return(document.getElementById('output').textContent = `You Win the round, Computer Loses! ${computerSelection} beats ${playerSelection}`);
+        playWin++;
+        return(document.getElementById('output').textContent = `You Win the round, Computer Loses! ${computerSelection} beats ${playerSelection}.`);
         
     }
 }
@@ -60,11 +56,24 @@ function wins(num, computerSelection, playerSelection){
 
     let compWin = 0;
     let playWin = 0;
+ 
 
+function display(compWin, playWin,num,playerSelection, computerSelection){
+    if(playWin === 3){
+        document.getElementById('restart').innerHTML =  "<button class = 'btnRestart'>Restart The Game</button>"
+        document.getElementById('game-win').textContent = "Player Wins the Game";
+        
+    }
+    else if (compWin === 3){
+        document.getElementById('restart').innerHTML =  "<button class = 'btnRestart'>Restart The Game</button>"
+        document.getElementById('game-win').textContent = "Computer Wins the Game";
+    }
+    else{
+        wins(num, playerSelection, computerSelection)
+    }  
+}
 
 const buttons = document.querySelectorAll('button');
-
-// we use the .forEach method to iterate through each button
 buttons.forEach((button) => {
   button.addEventListener('click', () => {
     let playerSelection = button.id;
@@ -72,24 +81,19 @@ buttons.forEach((button) => {
     document.getElementById('player').textContent = playerSelection;
     document.getElementById('computer').textContent = computerSelection;
 
-    let num = playRound(playerSelection,computerSelection);  
+    let num = playRound(playerSelection,computerSelection);
 
+    display(compWin, playWin,num,playerSelection, computerSelection)
+ 
 
-    if(playWin === 3){
-        document.getElementById('game-win').textContent = "Player Wins the Game";
-    }
-    else if (compWin === 3){
-        document.getElementById('game-win').textContent = "Computer Wins the Game";
-    }
-    else{
-        wins(num, playerSelection, computerSelection)
-    }
-    
-
-
-    
 document.getElementById('player-win').textContent = playWin;
 document.getElementById('computer-win').textContent = compWin;
-  })
+
+
+
+})
 });
 
+
+const btn = document.querySelector('#restart');
+btn.addEventListener('click', () => {window.location.reload()});
