f042e4117e8a0f18a1a6ab863bd31795380d03b1
wasih790 <120667351+wasih790@users.noreply.github.com>
2023-02-16 13:13:27 -0500

    Created a counter to display players wins

--- external.js
+++ external.js
@@ -17,21 +17,49 @@
             if ((playerSelection === "Rock" &&  computerSelection === "Rock") || 
             (playerSelection === "Paper" &&  computerSelection === "Paper") ||
             ((playerSelection === "Scissors" || playerSelection === "Scissor") &&  computerSelection === "Scissors")){
-                return("Tie for the round");
+                return(0);
             }
             else if ((playerSelection === "Rock" &&  computerSelection === "Paper") || 
             (playerSelection === "Paper" &&  computerSelection === "Scissors") ||
             ((playerSelection === "Scissors" || playerSelection === "Scissor") &&  computerSelection === "Rock")){
-               return (`Computer Wins the round, You Lose! ${computerSelection} beats ${playerSelection}`);
+               return (1);
             }
             else if ((computerSelection === "Rock" &&  playerSelection === "Paper") || 
             (computerSelection === "Paper" &&  (playerSelection === "Scissors" || playerSelection === "Scissor")) ||
             (computerSelection === "Scissors" &&  playerSelection === "Rock")){
-               return(`You Win the round, Computer Loses! ${playerSelection} beats ${computerSelection}`)
+               return(2)
             }
     }
 
 
+    // return("Tie for the round");
+    // return (`Computer Wins the round, You Lose! ${computerSelection} beats ${playerSelection}`);
+    // return(`You Win the round, Computer Loses! ${playerSelection} beats ${computerSelection}`)
+
+
+function wins(num, computerSelection, playerSelection){
+    if(num === 0){
+        return (document.getElementById('output').textContent = "Tie for the round");
+       
+    }
+    else if (num === 1){
+        compWin++;
+       return( document.getElementById('output').textContent = `Computer Wins the round, You Lose! ${computerSelection} beats ${playerSelection}`);
+        
+      
+    }
+    else if (num === 2){
+        playWin++;
+        return(document.getElementById('output').textContent = `You Win the round, Computer Loses! ${playerSelection} beats ${computerSelection}`);
+        
+    }
+}
+
+
+
+
+    let compWin = 0;
+    let playWin = 0;
 
 
 const buttons = document.querySelectorAll('button');
@@ -39,10 +67,23 @@ const buttons = document.querySelectorAll('button');
 // we use the .forEach method to iterate through each button
 buttons.forEach((button) => {
   button.addEventListener('click', () => {
-    playerSelection = button.id;
-    computerSelection = getComputerChoice(randNum())
+    let playerSelection = button.id;
+    let computerSelection = getComputerChoice(randNum())
     document.getElementById('player').textContent = playerSelection;
     document.getElementById('computer').textContent = computerSelection;
-    document.getElementById('output').textContent =  playRound(playerSelection,computerSelection);
+
+    let num = playRound(playerSelection,computerSelection);  
+
+
+
+
+    wins(num, playerSelection, computerSelection)
+
+
+
+    
+document.getElementById('player-win').textContent = playWin;
+document.getElementById('computer-win').textContent = compWin;
   })
 });
+
